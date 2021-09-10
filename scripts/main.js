b34b22cfb39a25ac3e0b660b9e84f67032f543fc
William Nigbur <7578002@isd535.org>
2021-09-10 09:41:27 -0500

    Removed the Shapes and replaced the code (Broken as of currently)

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,22 @@
 let canvas = document.getElementById("My Canvas");
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+let x = cavan.width/2;
+let y = canvas.height-30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx;strokeStyle = "rgba(0, 0, 255, 0.5)"
-ctx.stroke();
-ctx.closepath();
+function draw() {
+    // drawing code
+    ctx.beginPath;
+    ctx.arc(x, y, 10, 0, MathP*I*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    // Change the x and y values for the ball
+
+    x += dx;
+    y =+ dy;
+}
+setInterval(draw,10);
