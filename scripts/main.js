4845f79f3381b867357dc74fea473d89c888f0a8
William Nigbur <7578002@isd535.org>
2021-09-15 09:44:14 -0500

    added non collision bricks but need to fix the width of the canvas

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,23 @@ let paddleX = (canvas.width-paddleWidth) / 2;
 let rightPressed = false;
 let leftPresed = false;
 
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+//set up a 2-dimetional array for the bricks
+let bricks = [];
+for (let c=0; c < brickColumnCount; c++){
+    bricks[c] = [];
+    for(let r=0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
 function drawBall() {
      ctx.beginPath();
      ctx.arc( x, y, ballRadius, 0, Math.PI*2);
@@ -33,9 +50,30 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for(let c=0; c < brickColumnCount; c++) {
+        for(let r=0; r < brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding));
+            var brickY = (r*(brickHeight+brickPadding));
+            bricks[c][r].x = 0;
+            bricks[c][r].y = 0;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
+
 function draw() {
     // clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // draw the bricks
+    drawBricks();
+
     // draw the ball
     drawBall();
 
@@ -45,12 +83,20 @@ function draw() {
     x += dx;
     y += dy;
    
-    //check to see if we've gone off hte edge of hit board
+    // check to see if we've gone off hte edge of hit board
     if (x > canvas.width - ballRadius || x < ballRadius) {
         dx = -dx;
     }
-    if(y > canvas.height - ballRadius || y < ballRadius) {
+    if (y > canvas.height - ballRadius || y < ballRadius) { //ceiling check 
         dy = -dy
+    }else if (y+dy > canvas.height-ballRadius) { //floor check
+        if(x > paddleX && x < paddleX + paddleWidth) { // paddle check
+            dy = -dy;
+        } else { // if hit the floor !
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval); // need browswer to end game
+        }
     }
 
     // paddle controls
@@ -92,4 +138,4 @@ function keyUpHandler(e) {
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-setInterval(draw,10);
+let interval = setInterval(draw,10);
